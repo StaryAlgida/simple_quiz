9a022a6d535af6709416d74288f5175f37bd5828
piotr_wawrzenczyk <p.wawrzenczyk1@gmail.com>
2023-02-04 19:02:04 +0100

    all done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import {useState} from 'react';
+import Data from "./components/quiz.json"
+import Data_pl from "./components/quiz-pl.json"
 function App() {
+  
+  // const [isActive, setIsActive] = useState(false);
+
+  // function check(e){
+  //   // console.log(e.target.className);
+  //   if(e.target.className === "true")
+  //   {
+  //     console.log("ok");
+  //   }
+  //   else
+  //   {
+  //     console.log("Nie ok");
+  //   } 
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <RenderQuestions/> */}
+
+        {
+          Data.map(d =>{
+            return(
+              <div key={d.id}>
+                <p>{d.question}</p>
+                <ul>
+                  {d.answers.map(answer => {
+                    if (d.correct_answer.includes(answer))
+                    {
+                      return(
+                      <li className="true" onClick={((e) => e.target.style.backgroundColor = "green")}>{answer}</li>
+                      )
+                    }     
+                    else
+                    {
+                      return(
+                      <li className="false" onClick={((e) => e.target.style.backgroundColor = "red")}>{answer}</li>
+                      )
+                    }       
+                    
+                })}
+                </ul>
+              </div>
+            )
+          })
+        }
+        
       </header>
     </div>
   );
